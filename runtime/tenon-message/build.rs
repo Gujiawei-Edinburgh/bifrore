fbf33d5811835463f5bf8ec5d1cdd0a7caaf2faa
Gu Jiawei <gujiaweijoe@foxmail.com>
2026-06-13 20:09:26 +0800

    1. add cross crates message definitions.

---
+++ runtime/tenon-message/build.rs
@@ -0,0 +1,8 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    prost_build::Config::new()
+        .compile_protos(
+            &["src/daemon-message.proto", "src/cp-message.proto"],
+            &["src"],
+        )?;
+    Ok(())
+}
---
+++ runtime/tenon-message/src/lib.rs
@@ -0,0 +1,13 @@
+pub mod daemon {
+    pub mod v1 {
+        include!(concat!(env!("OUT_DIR"), "/tenon.daemon.v1.rs"));
+    }
+}
+
+pub mod cp {
+    pub mod v1 {
+        include!(concat!(env!("OUT_DIR"), "/tenon.cp.v1.rs"));
+    }
+}
+
+pub mod codec;
